88325c2458e17ca5767c4136ebe85b6feca5d9c7
Ahmed Essam <ahmedessamsaperr156@gmail.com>
2026-04-12 01:20:36 +0200

    core files

---
+++ app/core/config.py
@@ -0,0 +1,15 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+class Settings:
+    DATABASE_URL: str = os.getenv("DATABASE_URL")
+
+    DB_HOST: str = os.getenv("DB_HOST")
+    DB_PORT: str = os.getenv("DB_PORT")
+    DB_NAME: str = os.getenv("DB_NAME")
+    DB_USER: str = os.getenv("DB_USER")
+    DB_PASSWORD: str = os.getenv("DB_PASSWORD")
+    
+settings = Settings()
